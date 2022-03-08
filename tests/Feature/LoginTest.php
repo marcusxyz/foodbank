aa51529a41e42cf928e94e4664351e41cdea5c5e
h-emma <emmah92@hotmail.com>
2022-03-08 15:06:33 +0100

    Add test for login

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_view_login_form()
+    {
+        $response = $this->get('/');
+        $response->assertSeeText('Email');
+        $response->assertStatus(200);
+    }
+}
