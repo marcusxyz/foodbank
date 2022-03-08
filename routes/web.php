c79d50f8463f37b17963dd31b21ec9c6c19b4e00
h-emma <emmah92@hotmail.com>
2022-03-08 09:33:26 +0100

    Add routes for login and logout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('login');
+Route::post('login', LoginController::class)->middleware('guest');
+Route::get('logout', LogoutController::class)->middleware('auth');
