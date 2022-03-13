e6a8512648dad1e8c6ddc3f98383bfbadf429bc8
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-03-13 10:07:52 +0100

    Update recipe tests now passes

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
@@ -11,8 +13,21 @@ class ProfileTest extends TestCase
 
     public function test_view_profile()
     {
-        $response = $this->get('login');
-        $response->assertSeeText('Email');
+        $user = new User();
+        $user->name = 'Mr Robot';
+        $user->email = 'example@yrgo.se';
+        $user->password = Hash::make('123');
+        $user->save();
+
+        $response = $this->actingAs($user)->get('user/profile');
+        $response->assertSeeText('My profile');
         $response->assertStatus(200);
+    }
+
+    public function test_view_profile_as_guest()
+    {
+        $response = $this->followingRedirects()->get('user/profile');
+        $response->assertStatus(200);
+        $response->assertSeeText('Create an account');
     }
 }
---
+++ tests/Feature/UpdateRecipeTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UpdateRecipeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_view_update_recipe()
+    {
+        // Generate dummy data for a user, not sure why this doesn't work..
+        // $user = User::factory()->create();
+
+        // Create new user manually instead
+        $user = new User();
+        $user->name = 'Mr Robot';
+        $user->email = 'example@yrgo.se';
+        $user->password = Hash::make('123');
+        $user->save();
+
+        // Generate dummy data for recipe
+        $recipe = Recipe::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->followingRedirects()
+            ->get("user/update/recipe:{$recipe->id}");
+
+        $response->assertSeeText('Update recipe');
+        $response->assertStatus(200);
+    }
+
+    public function test_update_recipe()
+    {
+        // Create new user manually
+        $user = new User();
+        $user->name = 'Mr Robot';
+        $user->email = 'example@yrgo.se';
+        $user->password = Hash::make('123');
+        $user->save();
+
+        // Generate dummy data for recipe
+        $recipe = Recipe::factory()->create();
+
+        // Manually update recipe
+        $attributes = [
+            'title' => 'mos',
+            'description' => 'mos',
+            'ingredients' => 'mos',
+            'recipe_steps' => 'mos',
+        ];
+
+        $request = $this
+            ->followingRedirects()
+            ->actingAs($user)
+            ->post("recipes.patch/{$recipe->id}", $attributes);
+
+        $this->assertDatabaseHas('recipes', $attributes);
+        $request->assertSeeText("Recipe has been updated!");
+        $request->assertStatus(200);
+    }
+}
