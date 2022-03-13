9c12b7adc407a93697c89273648afe929e65091a
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-03-13 10:32:27 +0100

    Added subheaders to recipe view

--- resources/views/recipes/view.blade.php
+++ resources/views/recipes/view.blade.php
@@ -1,5 +1,10 @@
 <h1>{{$recipes->title}}</h1>
 
+<h3>Description</h3>
 <p>{{$recipes->description}}</p>
 
+<h3>Ingredients</h3>
 <p>{{$recipes->ingredients}}</p>
+
+<h3>Steps</h3>
+<p>{{$recipes->recipe_steps}}</p>
