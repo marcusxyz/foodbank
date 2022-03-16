151907d820b8a15434a46d7e2dd43beb1e56cc17
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2022-03-16 07:16:32 +0100

    keep old input values after validation for name and email on register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,11 +7,11 @@
     @csrf
     <div>
         <label for="name">Name</label>
-        <input name="name" id="name" type="name" />
+        <input name="name" id="name" type="name" value="{{ old('name') }}" />
     </div>
     <div>
         <label for="email">Email</label>
-        <input name="email" id="email" type="email" />
+        <input name="email" id="email" type="email" value="{{ old('email') }}" />
     </div>
     <div>
         <label for="password">Password</label>
